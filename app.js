86c928af9d6fee9e9ca00faf4d85d74ec1d22991
user <git@example.com>
2019-05-09 13:20:40 +0300

    CORS fix

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const TodoDatabase = require('./src/TodoDatabase');
 
 app.use('/test/main.js', express.static(__dirname + '/test/main.js'));
+
+app.use(cors({credentials: true, origin: true}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
